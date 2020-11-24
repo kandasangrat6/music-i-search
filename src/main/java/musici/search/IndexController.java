e4f90172b9ed95ba2e0df8d5fa23368ac0ed93df
kandasangrat6 <kandasangrat6@gmail.com>
2020-11-24 16:44:46 +0700

    init test page

--- src/main/java/musici/search/IndexController.java
+++ src/main/java/musici/search/IndexController.java
@@ -8,7 +8,8 @@ public class IndexController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello! 👋 Welcome to music-i-serach project!";
+		return "Hello! 👋 Welcome to music-i-serach project!<br /><br />" + 
+            "<h3><a href='/test'>Test Page</a></h3>";
 	}
 
 }
--- src/main/java/musici/search/TestController.java
+++ src/main/java/musici/search/TestController.java
@@ -1,14 +1,17 @@
 package musici.search;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class TestController {
 
-	@RequestMapping("/test")
-	public String test() {
-		return "👋";
+  @GetMapping(value = "/test")
+	public String test(@RequestParam(name="name", required=false, defaultValue="Test Page") String name, Model model) {
+		model.addAttribute("name", name);
+		return "test";
 	}
 
 }
